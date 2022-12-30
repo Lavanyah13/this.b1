80d1b9206850d43ad63cb39e04b4f7bc1d381436
Lavanyah B <104270776+Lavanyah13@users.noreply.github.com>
2022-12-30 22:46:52 +0530

    Create oops.java

---
+++ oops.java
@@ -0,0 +1,11 @@
+class Student{
+    private int roll_no;
+    String name;
+}
+class DPS {
+        public static void main (String[] args) {
+        Student s=new Student();
+        s.name="Neha";
+        System.out.println(s.name);
+    }
+}
